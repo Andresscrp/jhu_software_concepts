12e6fadb1ea5e69c93e7b885f775f1b54abf0e76
Andres Sanchez-Castellanos <andresscrp@gmail.com>
2026-01-25 18:50:53 -0500

    Module 1: Flask personal website

--- module_1/personal_website/__init__.py
+++ module_1/personal_website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from personal_website import pages
 
+##Creates blueprint that will store the main pages of the website
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages.bp)
--- module_1/personal_website/pages.py
+++ module_1/personal_website/pages.py
@@ -2,11 +2,25 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("pages", __name__)
 
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+##Renders the pages on the website
 @bp.route("/")
 def home():
-    return render_template("pages/home.html")
+    return render_template("pages/about.html")
 
 @bp.route("/about")
 def about():
-    return render_template("pages/about.html")
+    return render_template("pages/about.html") 
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
+
 
